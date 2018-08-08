d2201bb3dcc4d5785de0d1995a6d80c201640abc
Demon Singur <demonsingur@gmail.com>
2018-08-08 19:57:04 +0000

    jason: recovery: include moved headers
    Change-Id: Ia0e49562dac01bf3ecd674270f2011546cfd3498

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,10 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-	bootable/recovery \
-	bootable/recovery/edify/include \
-	bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_jason
+LOCAL_STATIC_LIBRARIES := libedify libotautil
 include $(BUILD_STATIC_LIBRARY)
