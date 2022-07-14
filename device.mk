639e08f07d50ea34c595bccf5918caa6b5a81f80
Alexandru Scurtu <sasha.scurtu@gmail.com>
2022-07-14 15:58:36 +0700

    jason: Build sensorservice 1.0 HAL system and vendor variant
    * E init    : Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 516 (/system/bin/hwservicemanager)
    
    Change-Id: If696de35b0d70e1f67d72be456347c6c7fd691f7
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- device.mk
+++ device.mk
@@ -113,6 +113,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_jason
 
+# Sensor
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
