f0d30c348ce394ef04f7909efaac81ed248c978f
Nanhumly <admin@warbler.cc>
2023-07-17 11:48:53 +0000

    jason: Switch to stock camera hal
    Co-authored-by: jimmy <quesoo@gmail.com>
    Co-authored-by: iamehsangh <ehsan.ghasab@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I4efb494a63f2b0a99eec83bebc3c07b520f2b27c
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- extract-files.sh
+++ extract-files.sh
@@ -8,8 +8,27 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/hw/camera.sdm660.so)
+            "${PATCHELF}" --replace-needed "libminikin.so" "libminikin-v28.so" "${2}"
+            ;;
+        vendor/lib/libarcsoft_beauty_shot.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib/libFaceGrade.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
         vendor/lib64/libgf_hal.so)
             "${PATCHELF}" --remove-needed "libpowermanager.so" "${2}"
+            ;;
+        vendor/lib/libicuuc-v28.so)
+            "${PATCHELF}" --set-soname "libicuuc-v28.so" "${2}"
+            ;;
+        vendor/lib/libminikin-v28.so)
+            "${PATCHELF}" --set-soname "libminikin-v28.so" "${2}"
+            ;;
+        vendor/lib/libMiCameraHal.so)
+            "${PATCHELF}" --replace-needed "libicuuc.so" "libicuuc-v28.so" "${2}"
+            "${PATCHELF}" --replace-needed "libminikin.so" "libminikin-v28.so" "${2}"
             ;;
         vendor/lib/libmmcamera2_stats_modules.so)
             "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
@@ -22,6 +41,9 @@ function blob_fixup() {
             "${PATCHELF}" --remove-needed "libgui.so" "${2}"
             ;;
         vendor/lib/libmpbase.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib/libVDClearShot.so)
             "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
             ;;
     esac
