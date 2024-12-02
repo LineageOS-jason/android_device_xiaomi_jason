96ea8e3c6ae8ba2e163aabe0107c59f2f9d988e0
Michael Bestas <mkbestas@lineageos.org>
2024-12-02 06:59:32 +0200

    jason: Convert librecovery_updater_jason Android.mk to bp
    Change-Id: Ib9e3a5859ed60c36c28969c9cf450d9af9d97690

--- recovery/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := recovery_updater.cpp
-LOCAL_MODULE := librecovery_updater_jason
-LOCAL_STATIC_LIBRARIES := libedify libotautil
-include $(BUILD_STATIC_LIBRARY)
