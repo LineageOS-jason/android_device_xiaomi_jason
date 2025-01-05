b361a31efdc239d60b7e2141829b96c0c3e5700a
Nanhumly <admin@warbler.cc>
2025-01-05 07:20:08 +0000

    jason: Patch camera blobs to appease elfchecks
    Change-Id: I7b8d18ca7193d7179a08d0352ddd7e72575b8aad

--- extract-files.py
+++ extract-files.py
@@ -23,14 +23,22 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib/libarcsoft_dualcam_optical_zoom.so',
         'vendor/lib/libarcsoft_dualcam_optical_zoom_control.so',
         'vendor/lib/libarcsoft_dualcam_refocus.so',
-        'vendor/lib/libmmcamera_hdr_gb_lib.so',
     ): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'vendor/lib/libmmcamera2_stats_modules.so': blob_fixup()
         .remove_needed('libandroid.so')
         .remove_needed('libgui.so'),
+    'vendor/lib/libmmcamera_hdr_gb_lib.so': blob_fixup()
+        .add_needed('liblog.so')
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'vendor/lib/libmmcamera_jason_s5k3p8sp_sunny.so': blob_fixup()
         .binary_regex_replace(b'\x1e\x40\x9a\x99\x99\x99\x99\x99\x3b\x40\x10', b'\x1e\x40\x9a\x99\x99\x99\x99\x99\x3b\x40\x01'),
+    (
+     'vendor/lib/libmmcamera_pdaf.so',
+     'vendor/lib/libmmcamera_pdafcamif.so',
+     'vendor/lib/libmmcamera_tintless_bg_pca_algo.so',
+    ): blob_fixup()
+        .add_needed('liblog.so'),
     'vendor/lib/libmmcamera_ppeiscore.so': blob_fixup()
         .remove_needed('libgui.so'),
     'vendor/lib/libmpbase.so': blob_fixup()
