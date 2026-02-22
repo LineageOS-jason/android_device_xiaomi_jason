62cdfb566b6f40ed54721f36212a345938fa01d0
Nanhumly <admin@warbler.cc>
2026-02-22 12:33:06 +0000

    fixup! jason: Implement ELF checks & swap to python extract

--- extract-files.py
+++ extract-files.py
@@ -18,10 +18,10 @@ from extract_utils.fixups_blob import (
 )
 
 namespace_imports = [
-    'hardware/xiaomi',
     'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/msm8998',
     'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
     'vendor/xiaomi/sdm660-common',
 ]
 
